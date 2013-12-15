040e103a3f3f34e1f2a2b34258f4442800673be1
probonopd <probono@puredarwin.org>
2013-12-15 18:37:53 +0000

    Update mymodule.lua
    Create a new item in the main menu bar

--- traits/audio/radio/files/usr/lib/lua/luci/controller/myapp/mymodule.lua
+++ traits/audio/radio/files/usr/lib/lua/luci/controller/myapp/mymodule.lua
@@ -7,11 +7,14 @@ module("luci.controller.myapp.mymodule", package.seeall)
 
 function index()
 
-    entry({"admin", "radio", "stations"}, cbi("myapp-mymodule/radio"), "Radio stations", 30).dependent=false
-    entry({"admin", "radio", "remote"}, cbi("myapp-mymodule/remote"), "Radio remote control", 30).dependent=false
+--  Create a new item in the main menu bar
+    entry({"admin", "radio"}, cbi("myapp-mymodule/radio"), _("Radio"), 21).dependent = true
+
+--  Attach the submenus to the main menu item
+    entry({"admin", "radio", "stations"}, cbi("myapp-mymodule/radio"), "Radio stations", 30).dependent=true
+    entry({"admin", "radio", "remote"}, cbi("myapp-mymodule/remote"), "Radio remote control", 31).dependent=true
 
 --    entry({"click", "here", "now"}, call("action_tryme"), "Click here", 10).dependent=false
 --    entry({"my", "new", "template"}, template("myapp-mymodule/helloworld"), "Hello world", 20).dependent=false
 
 end
-
